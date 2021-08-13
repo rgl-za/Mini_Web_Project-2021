5b58cdee7bbc76693949313aad60f249f78ea91d
maie421 <maie421@naver.com>
2021-08-13 21:55:53 +0900

    no message

---
+++ Mini_Web_Project-/src/main/java/Mini_Web_Project2021/Mini_Web_Project/MiniWebProjectApplication.java
@@ -0,0 +1,13 @@
+package Mini_Web_Project2021.Mini_Web_Project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniWebProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiniWebProjectApplication.class, args);
+	}
+
+}
---
+++ Mini_Web_Project-/src/test/java/Mini_Web_Project2021/Mini_Web_Project/MiniWebProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package Mini_Web_Project2021.Mini_Web_Project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniWebProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
