43405d4369a34bdf72ff03eae0bdddace6999b8e
jin0220 <37447282+jin0220@users.noreply.github.com>
2021-08-14 01:01:10 +0900

    item 테이블 생성

---
+++ Mini_Web_Project-/src/main/java/Mini_Web_Project2021/Mini_Web_Project/domain/item.java
@@ -0,0 +1,22 @@
+package Mini_Web_Project2021.Mini_Web_Project.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+@Getter
+@Setter
+public class item {
+    @Id
+    @GeneratedValue
+    @Column(name = "item_id")
+    private Long num;
+    private String name; //상품명
+    private int price; //가격
+    private int quantity; //수량
+}
