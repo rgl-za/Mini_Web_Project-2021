57caacf01181f5faad9d6adb5c9def4b680bf4b2
maie421 <maie421@naver.com>
2021-08-14 16:09:57 +0900

    추가

--- Mini_Web_Project-/src/main/java/Mini_Web_Project2021/Mini_Web_Project/repository/MemberRepository.java
+++ Mini_Web_Project-/src/main/java/Mini_Web_Project2021/Mini_Web_Project/repository/MemberRepository.java
@@ -3,6 +3,7 @@ package Mini_Web_Project2021.Mini_Web_Project.repository;
 import Mini_Web_Project2021.Mini_Web_Project.domain.Member;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
@@ -14,6 +15,8 @@ public class MemberRepository {
     @PersistenceContext
     private EntityManager em;
 
+
+    @Transactional
     public Long join(Member member) {
         em.persist(member);
         return member.getId();
