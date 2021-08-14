37c508d69f387c940cf09905369b87525423e079
maie421 <maie421@naver.com>
2021-08-14 15:37:03 +0900

    멤버 repository

---
+++ Mini_Web_Project-/src/main/java/Mini_Web_Project2021/Mini_Web_Project/repository/MemberRepository.java
@@ -0,0 +1,25 @@
+package Mini_Web_Project2021.Mini_Web_Project.repository;
+
+import Mini_Web_Project2021.Mini_Web_Project.domain.Member;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+@Repository
+@RequiredArgsConstructor
+public class MemberRepository {
+
+    @PersistenceContext
+    private EntityManager em;
+
+    public Long join(Member member) {
+        em.persist(member);
+        return member.getId();
+    }
+
+    public Member find(Long id) {
+        return em.find(Member.class, id);
+    }
+}
